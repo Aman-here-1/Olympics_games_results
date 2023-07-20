e9398bdd21c4fb01fb0daa578cc61036f0e5d682
Aman Chouhan <139533493+Aman-here-1@users.noreply.github.com>
2023-07-20 16:37:52 +0530

    Add files via upload

--- Olympics.sql
+++ Olympics.sql
@@ -1,19 +1,31 @@
-SQL Queries:
-1. How many Olympics games have been held?
-Problem Statement: Write a SQL query to find the total no of Olympic Games held as per the dataset.
-Solution: Select count(Distinct(games)) as Total_sports from OLYMPICS_HISTORY
-2. List down all Olympics games held so far.
-Problem Statement: Write a SQL query to list down all the Olympic Games held so far.
-Solution: select oh.year, oh.season, city from olympics_history as oh
-3. Mention the total no of nations who participated in each olympics game?
-Problem Statement: SQL query to fetch total no of countries participated in each olympic games.
-Solution: select oh.games, count(distinct(ohr.region)) as total_countries from olympics_history as oh
+--1. How many Olympics games have been held?
+--Problem Statement: Write a SQL query to find the total no of Olympic Games held as per the dataset.
+--Solution: 
+
+Select count(Distinct(games)) as Total_sports from OLYMPICS_HISTORY
+
+--2. List down all Olympics games held so far.
+--Problem Statement: Write a SQL query to list down all the Olympic Games held so far.
+
+--Solution: 
+select oh.year, oh.season, city from olympics_history as oh
+
+--3. Mention the total no of nations who participated in each olympics game?
+--Problem Statement: SQL query to fetch total no of countries participated in each olympic games.
+
+--Solution: 
+
+select oh.games, count(distinct(ohr.region)) as total_countries from olympics_history as oh
 inner join olympics_history_noc_regions ohr
 on oh.noc = ohr.noc
 group by oh.games
-11. Fetch the top 5 athletes who have won the most gold medals.
-Problem Statement: SQL query to fetch the top 5 athletes who have won the most gold medals.
-Solution: select * from (
+
+--11. Fetch the top 5 athletes who have won the most gold medals.
+--Problem Statement: SQL query to fetch the top 5 athletes who have won the most gold medals.
+
+--Solution: 
+
+select * from (
 select *,
 dense_rank() over (order by Total_gold_medal desc) as r
 from (SELECT name, team, count(medal) as Total_gold_medal FROM olympics_history 
@@ -22,11 +34,16 @@ group by name, team
 order by count(medal) desc
 ) as a1) as a2
 where r <=5
-14. List down total gold, silver and bronze medals won by each country.
-Problem Statement: Write a SQL query to list down the total gold, silver and bronze medals won by each country
-CROSSTAB – transpose column to row
-Coalesce – to avoid null values
-Solution: select country,
+
+
+--14. List down total gold, silver and bronze medals won by each country.
+--Problem Statement: Write a SQL query to list down the total gold, silver and bronze medals won by each country
+--CROSSTAB – transpose column to row
+--Coalesce – to avoid null values
+--Solution: 
+
+
+select country,
 coalesce (gold, 0) as gold,
 coalesce (silver, 0) as silver,
 coalesce (bronze, 0) as bronze
